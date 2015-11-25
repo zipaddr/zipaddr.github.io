6c05e8d0c0e75d237ed2fe2fba25ae7d4cf14aef
ta.terunuma <ta.terunuma@gmail.com>
2015-11-25 16:17:31 +0900

    cscart_1.2rr

--- cscart.js
+++ cscart.js
@@ -9,11 +9,13 @@ function zipaddr_ownb(){
   var k3= "company_data[";
   var k4= "supplier_data[";
   var k5= "update[";
+  var k6= "field__default_";
   D.pm[1]={"zip":k1+zp,   "zip1":"", "pref":k1+st,   "city":"", "addr":k1+ci,    "focus":k1+ad   };//¿‹æ
   D.pm[2]={"zip":k2+zp,   "zip1":"", "pref":k2+st,   "city":"", "addr":k2+ci,    "focus":k2+ad   };//”z‘—æ
   D.pm[3]={"zip":k3+zp,   "zip1":"", "pref":k3+st,   "city":"", "addr":k3+ci,    "focus":k3+ad   };
   D.pm[4]={"zip":k4+zp,   "zip1":"", "pref":k4+st,   "city":"", "addr":k4+ci,    "focus":k4+ad   };
   D.pm[5]={"zip":k5+"33]","zip1":"", "pref":k5+"25]","city":"", "addr":k5+"26]", "focus":k5+"27]"};//shop
+  D.pm[6]={"zip":k6+"zipcode_18","zip1":"", "pref":k6+"state_21","city":"", "addr":k6+"city_19", "focus":k6+"address_17"};//default
 }
 function AjaxZip3(){}
 AjaxZip3.zip2addr=function(z, d1,s,c,d2,a){Zip.aa(z);};
