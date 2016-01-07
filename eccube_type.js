19badafabebed63d96b99b1ae96206fdccbdc657
ta.terunuma <ta.terunuma@gmail.com>
2016-01-07 20:13:28 +0900

    eccube_type_1.3

--- eccube_type.js
+++ eccube_type.js
@@ -1,5 +1,5 @@
 function typeauto_ownb(){
-	var ver= "1.2";
+	var ver= "1.3";
 //tel,fax（お問い合わせ,会員登録,お客様情報,お届け先,admin会員管理,admin受注管理）
 var idg= "contact_tel_tel0,entry_tel_tel0,entry_fax_fax0,nonmember_tel_tel0";
 	idg+=",shopping_shipping_tel_tel0,shopping_shipping_fax_fax0";
@@ -13,5 +13,6 @@ var idg= "contact_tel_tel0,entry_tel_tel0,entry_fax_fax0,nonmember_tel_tel0";
 			n++;
 		}
 	}
+	if( typeof typeauto_ownb_continue==="function" ) typeauto_ownb_continue(); // 追加owncode
 }
-//変更する場合は、自サイトに設置して下さい。
+//拡張する場合は、自サイトにtypeauto_ownb_continueを設置して下さい。
