c8a6f2c2b25541e1afb8b683a795a1d11d4c2163
ta.terunuma <ta.terunuma@gmail.com>
2016-09-04 06:00:13 +0900

    eccube.js_1.4

--- eccube.js
+++ eccube.js
@@ -1,4 +1,4 @@
-function zipaddr_ownb(){var ver="1.3";
+function zipaddr_ownb(){var ver="1.4";
   var zp= "zip01";
   var z2= "zip02";
   var st= "pref";
@@ -9,13 +9,13 @@ function zipaddr_ownb(){var ver="1.3";
   var k3= "order_address_";
   var k4= "admin_customer_zip_";
   var k5= "admin_customer_address_";
-  D.pm[1]={"zip":   zp, "zip1":   z2,  "pref":   st,  "city":"",     "addr":   ad,  "focus":   fc};
-  D.pm[2]={"zip":k2+zp, "zip1":k2+z2,  "pref":k3+st,  "city":"",     "addr":k3+ad,  "focus":k3+fc};
-  D.pm[3]={"zip":k4+zp, "zip1":k4+z2,  "pref":k5+st,  "city":"",     "addr":k5+ad,  "focus":k5+fc};
-  D.pm[4]={"zip":"zip", "zip1":"zip1", "pref":"pref", "city":"city", "addr":"addr", "focus":"addr"};
-  D.pm[5]={"zip":"zip2","zip1":"zip21","pref":"pref2","city":"city2","addr":"addr2","focus":"addr2"};
-  D.pm[6]={"zip":"zip3","zip1":"zip31","pref":"pref3","city":"city3","addr":"addr3","focus":"addr3"};
-//D.pm[7]={"zip":"〒",  "zip1":"下4桁","pref":"都道府県","city":"市区町村", "addr":"住所","focus":"focus"};
+  D.pm[1]={"zip":   zp, "zip1":   z2,  "pref":   st,  "city":"",     "area":   ad,  "addr":   fc,  "focus":   fc};
+  D.pm[2]={"zip":k2+zp, "zip1":k2+z2,  "pref":k3+st,  "city":"",     "area":k3+ad,  "addr":k3+fc,  "focus":k3+fc};
+  D.pm[3]={"zip":k4+zp, "zip1":k4+z2,  "pref":k5+st,  "city":"",     "area":k5+ad,  "addr":k5+fc,  "focus":k5+fc};
+  D.pm[4]={"zip":"zip", "zip1":"zip1", "pref":"pref", "city":"city", "area":"area", "addr":"addr", "focus":"addr"};
+  D.pm[5]={"zip":"zip2","zip1":"zip21","pref":"pref2","city":"city2","area":"area2","addr":"addr2","focus":"addr2"};
+  D.pm[6]={"zip":"zip3","zip1":"zip31","pref":"pref3","city":"city3","area":"area3","addr":"addr3","focus":"addr3"};
+//D.pm[7]={"zip":"〒",  "zip1":"下4桁","pref":"都道府県","city":"市区町村","area":"地区","addr":"番地","focus":"focus"};
 
   D.sfon="16";      // sPhone
   D.shig="1.5";
--- eccube2.js
+++ eccube2.js
@@ -1,4 +1,4 @@
-function zipaddr_ownb(){var ver="1.1";
+function zipaddr_ownb(){var ver="1.2";
   D.zipmax= 20;
   var zp= "zip01";
   var z2= "zip02";
@@ -11,20 +11,21 @@ function zipaddr_ownb(){var ver="1.1";
   var k4= "shipping_";
   var k5= "law_";
   var k6= "dmy_";
-  D.pm[1]={"zip":   zp, "zip1":   z2, "pref":   st, "city":"", "addr":   ad, "focus":   fc};
-  D.pm[2]={"zip":k2+zp, "zip1":k2+z2, "pref":k2+st, "city":"", "addr":k2+ad, "focus":k2+fc};
-  D.pm[3]={"zip":k3+zp, "zip1":k3+z2, "pref":k3+st, "city":"", "addr":k3+ad, "focus":k3+fc};
-  D.pm[4]={"zip":k4+zp, "zip1":k4+z2, "pref":k4+st, "city":"", "addr":k4+ad, "focus":k4+fc};
-  D.pm[5]={"zip":k5+zp, "zip1":k5+z2, "pref":k5+st, "city":"", "addr":k5+ad, "focus":k5+fc};
-  D.pm[6]={"zip":k6+zp, "zip1":k6+z2, "pref":k6+st, "city":"", "addr":k6+ad, "focus":k6+fc};
+  D.pm[1]={"zip":   zp, "zip1":   z2, "pref":   st, "city":"", "area":   ad, "addr":   fc, "focus":   fc};
+  D.pm[2]={"zip":k2+zp, "zip1":k2+z2, "pref":k2+st, "city":"", "area":k2+ad, "addr":k2+fc, "focus":k2+fc};
+  D.pm[3]={"zip":k3+zp, "zip1":k3+z2, "pref":k3+st, "city":"", "area":k3+ad, "addr":k3+fc, "focus":k3+fc};
+  D.pm[4]={"zip":k4+zp, "zip1":k4+z2, "pref":k4+st, "city":"", "area":k4+ad, "addr":k4+fc, "focus":k4+fc};
+  D.pm[5]={"zip":k5+zp, "zip1":k5+z2, "pref":k5+st, "city":"", "area":k5+ad, "addr":k5+fc, "focus":k5+fc};
+  D.pm[6]={"zip":k6+zp, "zip1":k6+z2, "pref":k6+st, "city":"", "area":k6+ad, "addr":k6+fc, "focus":k6+fc};
   for( j=7;j<=D.zipmax;j++ ) { // 届け先はmax13か所
 	var n= j - 7;
 	var p= k4+zp +"["+n+"]";
 	var q= k4+z2 +"["+n+"]";
 	var r= k4+st +"["+n+"]";
+	var e= "";
 	var a= k4+ad +"["+n+"]";
 	var f= k4+fc +"["+n+"]";
-	D.pm[j]={"zip":p, "zip1":q, "pref":r, "city":"", "addr":a, "focus":f};
+	D.pm[j]={"zip":p, "zip1":q, "pref":r, "city":"", "area":a, "addr":f, "focus":f};
   }
   D.top=  21;            // 位置
   D.sl=  "都道府県を選択";
