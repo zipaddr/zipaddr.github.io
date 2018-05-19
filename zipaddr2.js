c0fdc2a52d9629b826e4378a960b2664a3d7d225
ta.terunuma <ta.terunuma@gmail.com>
2018-05-19 09:00:15 +0900

    zipaddr.css_v2

---
+++ zipaddr2.js
@@ -0,0 +1,49 @@
+@charset "utf-8";
+
+#autozip {          /* PC */
+	position: absolute;
+	font-family:'ヒラギノ角ゴ Pro W3','Hiragino Kaku Gothic Pro','メイリオ',Meiryo,'ＭＳ Ｐゴシック',sans-serif;
+	background: #009999;
+	font-weight: normal;
+	padding: 5px 8px !important;
+	border-radius: 8px;
+	z-index: 999999;
+	font-size: 12px;
+	line-height: 130%;
+}
+#autozips {         /* SmartPhone */
+	position: absolute;
+	font-family:'ヒラギノ角ゴ Pro W3','Hiragino Kaku Gothic Pro','メイリオ',Meiryo,'ＭＳ Ｐゴシック',sans-serif;
+	background: #009999;
+	font-weight: normal;
+	padding: 5px 8px !important;
+	border-radius: 8px;
+	z-index: 999999;
+	font-size: 20px;
+	line-height: 150%;
+}
+
+
+#zip_head {         /* 都道府県,市区町村 */
+	color: #ffffcc;
+}
+#zip_cnt {          /* Hit件数 */
+	color: #ffffcc;
+}
+.zip_line {         /* 地域部分 */
+	color: #ffffcc;
+}
+#zip_close {        /* 閉じる */
+	color: #333333;
+}
+#zip_vers {
+	color: #333333;
+}
+#zip_vers a {
+	color: #333333;
+	text-decoration: none;
+}
+#zip_vers a:hover {
+	color: #333333;
+	text-decoration: none;
+}
