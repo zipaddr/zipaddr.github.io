35e9a69348ae55970cc3e85c20097778b7379136
ta_terunuma <ta.terunuma@gmail.com>
2021-06-13 05:12:01 +0900

    wpforms.js_1.0

---
+++ wpforms.js
@@ -0,0 +1,27 @@
+function zipaddr_ownpm(){     //WPForms用
+	const ver="1.0";
+	let lab= new Array();
+	lab[0]= "郵便番号";
+	lab[1]= "都道府県";
+	lab[2]= "市区町村";
+	lab[3]= "住所";
+	let ans= new Array();
+	ans[0]= "";
+	ans[1]= "";
+	ans[2]= "";
+	ans[3]= "";
+	const elm= document.getElementsByTagName("label");
+	for( let ii=0;ii<elm.length;ii++ ){
+		const idx= elm[ii].id;
+		let dat= elm[ii].innerHTML; // <label...>都道府県  </label>
+		for( let jj=0;jj<lab.length;jj++ ){
+			if( dat.indexOf(lab[jj]) ){  //都道府県含まれる？
+				ans[jj]= idx.replace('-container','');
+				jj= 9999;
+		}	}
+	}
+//Set
+	pm= new Array();
+	pm[1]= {"zip":ans[0], "zip1":"", "pref":ans[1], "city":ans[2], "area":"", "addr":ans[3], "focus":""};
+	return pm;
+}
--- wpmembers.js
+++ wpmembers.js
@@ -1,12 +1,13 @@
 function zipaddr_ownpm(){     //WP-Members用
-  const ver="1.1";
-  const bbb= "billing_";
-  const zzc= bbb+ "postcode";
-  const zpf= bbb+ "state";
-  const zci= bbb+ "city";
-  const zad= bbb+ "address_1";
-  pm= new Array();
-  pm[1]= {"zip":zzc,  "zip1":"","pref":zpf,    "city":zci,   "area":"","addr":zad,    "focus":""}; // 項目idセット
-  pm[2]= {"zip":"zip","zip1":"","pref":"state","city":"city","area":"","addr":"addr1","focus":""};
-  return pm;
+	const ver="1.1";
+	const bbb= "billing_";
+	const zzc= bbb+ "postcode";
+	const zpf= bbb+ "state";
+	const zci= bbb+ "city";
+	const zad= bbb+ "address_1";
+//Set
+	pm= new Array();
+	pm[1]= {"zip":zzc,  "zip1":"","pref":zpf,    "city":zci,   "area":"","addr":zad,    "focus":""};
+	pm[2]= {"zip":"zip","zip1":"","pref":"state","city":"city","area":"","addr":"addr1","focus":""};
+	return pm;
 }
