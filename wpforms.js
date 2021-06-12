bba73e3de0f5dbbf39dba6babec6fb5142a858b4
ta_terunuma <ta.terunuma@gmail.com>
2021-06-13 05:58:54 +0900

    wpforms.js_1.1

--- wpforms.js
+++ wpforms.js
@@ -1,5 +1,5 @@
 function zipaddr_ownpm(){     //WPForms用
-	const ver="1.0";
+	const ver="1.1";
 	let lab= new Array();
 	lab[0]= "郵便番号";
 	lab[1]= "都道府県";
@@ -12,14 +12,15 @@ function zipaddr_ownpm(){     //WPForms用
 	ans[3]= "";
 	const elm= document.getElementsByTagName("label");
 	for( let ii=0;ii<elm.length;ii++ ){
-		const idx= elm[ii].id;
+		const idx= elm[ii].for;
 		let dat= elm[ii].innerHTML; // <label...>都道府県  </label>
 		for( let jj=0;jj<lab.length;jj++ ){
 			if( dat.indexOf(lab[jj]) ){  //都道府県含まれる？
-				ans[jj]= idx.replace('-container','');
+				ans[jj]= idx;
 				jj= 9999;
 		}	}
 	}
+alert( ans[0]+"##" );
 //Set
 	pm= new Array();
 	pm[1]= {"zip":ans[0], "zip1":"", "pref":ans[1], "city":ans[2], "area":"", "addr":ans[3], "focus":""};
