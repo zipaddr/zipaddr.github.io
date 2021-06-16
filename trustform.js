9cd5152c9e512bfbbf2d1daa92a447e7709d8b45
ta_terunuma <ta.terunuma@gmail.com>
2021-06-16 15:58:03 +0900

    trustform.js_1.2

--- trustform.js
+++ trustform.js
@@ -1,5 +1,5 @@
 function zipaddr_ownpm(){     //TrustForm用
-	const ver="1.1";
+	const ver="1.2";
 	let ans= new Array();
 	ans[0]= "";
 	ans[1]= "";
@@ -10,20 +10,20 @@ function zipaddr_ownpm(){     //TrustForm用
 	const dac= "zip_zip1_pref_city_area_addr";
 	const dan= dac.split("_");
 	for( let ii=0;ii<dan.length;ii++ ){
-		ans[jj]= zipaddr_trust( dan[jj] );
+		ans[ii]= zipaddr_trust( dan[ii] );
 	}
 //Set
 	pm= new Array();
 	pm[1]= {"zip":ans[0], "zip1":ans[1], "pref":ans[2], "city":ans[3], "area":ans[4], "addr":ans[5], "focus":""};
 	return pm;
 }
-zipaddr_trust=function(nam){
+function zipaddr_trust(nam){
 	let ans= nam;
 	if( nam != "" ){
 		const elm= document.getElementsByClassName(nam);
 		if( elm.length==1 ){
-			if( elm[0].id=="" ) ans= nam;
-			else                ans= elm[0].id;
+			if( elm[0].id=="" ) elm[0].id= nam;
+			else ans= elm[0].id;
 		}
 	}
 	return ans;
