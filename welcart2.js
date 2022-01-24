e83d0ddc55167922dd3c1d3528150a863e2b6d38
ta_terunuma <ta.terunuma@gmail.com>
2022-01-24 17:26:43 +0900

    welcart2.js

---
+++ welcart2.js
@@ -0,0 +1,9 @@
+function zipaddr_ownpm(){     //Welcart用
+	const ver="1.0";
+//Set
+	if( typeof ZP.sei != "undefined" )  ZP.sei="name1";
+	if( typeof ZP.mei != "undefined" )  ZP.mei="name2";
+	if( typeof ZP.tel != "undefined")ZP.tel[0]="tel";
+	pm= new Array();
+	return pm;
+}
