91fd6bf6bc028049663bddb2e407be4cc8078e89
ta_terunuma <ta.terunuma@gmail.com>
2022-02-01 11:42:20 +0900

    wpmembers.js_1.2

--- wpmembers.js
+++ wpmembers.js
@@ -1,11 +1,14 @@
 function zipaddr_ownpm(){     //WP-Members用
-	const ver="1.1";
+	const ver="1.2";
 	const bbb= "billing_";
 	const zzc= bbb+ "postcode";
 	const zpf= bbb+ "state";
 	const zci= bbb+ "city";
 	const zad= bbb+ "address_1";
 //Set
+	if( typeof ZP.tel != "undefined" ) {ZP.tel[1]= bbb+ "phone";}
+	if( typeof ZP.sei != "undefined" ) {ZP.sei[1]= "last_name";}
+	if( typeof ZP.mei != "undefined" ) {ZP.mei[1]= "first_name";}
 	pm= new Array();
 	pm[1]= {"zip":zzc,  "zip1":"","pref":zpf,    "city":zci,   "area":"","addr":zad,    "focus":""};
 	pm[2]= {"zip":"zip","zip1":"","pref":"state","city":"city","area":"","addr":"addr1","focus":""};
