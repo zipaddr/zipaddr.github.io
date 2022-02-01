3db4e5775d68c98ced49c43c4e42050d3aa2996b
ta_terunuma <ta.terunuma@gmail.com>
2022-02-01 10:35:22 +0900

    wpforms.js_1.5

--- wpforms.js
+++ wpforms.js
@@ -1,31 +1,29 @@
 function zipaddr_ownpm(){     //WPForms用
-	const ver="1.4";
-	let lab= new Array();
-	lab[0]= "郵便番号";
-	lab[1]= "都道府県";
-	lab[2]= "市区町村";
-	lab[3]= "住所";
-	lab[4]= "区市町村";
+	const ver="1.5";
 	let ans= new Array();
-	ans[0]= "";
-	ans[1]= "";
-	ans[2]= "";
-	ans[3]= "";
-	const elm= document.getElementsByTagName("label");
-	for( let ii=0;ii<elm.length;ii++ ){
-		const idx= elm[ii].htmlFor;
-		const dat= elm[ii].innerHTML; // <label...>都道府県  </label>
-		for( let jj=0;jj<lab.length;jj++ ){
-			const ptrn= new RegExp( lab[jj], 'g' );
-			matchArr= dat.match(ptrn);
-			if( Array.isArray(matchArr) ){  //都道府県含まれる？
-				if( jj==4 ) ans[2]= idx;
-				else       ans[jj]= idx;
-				jj= 9999;
-		}	}
-	}
+	ans[0]= zipaddr_ownpm_tag("label","郵便番号");
+	ans[1]= zipaddr_ownpm_tag("label","都道府県");
+	ans[2]= zipaddr_ownpm_tag("label","市区町村");
+	ans[3]= zipaddr_ownpm_tag("label","住所");
 //Set
+	if( typeof ZP.tel != "undefined" ) ZP.tel[1]= zipaddr_ownpm_tag("label","電話番号");
+	if( typeof ZP.sei != "undefined" ) ZP.sei[1]= zipaddr_ownpm_tag("label","姓");
+	if( typeof ZP.mei != "undefined" ) ZP.mei[1]= zipaddr_ownpm_tag("label","名");
 	pm= new Array();
 	pm[1]= {"zip":ans[0], "zip1":"", "pref":ans[1], "city":ans[2], "area":"", "addr":ans[3], "focus":""};
 	return pm;
+}
+function zipaddr_ownpm_tag(tag,names){
+	let ans="";
+	const ptrn= new RegExp(names);
+	const elm= document.getElementsByTagName(tag);
+	for( let ii=0;ii<elm.length;ii++ ){
+		let dat= elm[ii].innerHTML;               // <label...>都道府県  </label>
+		dat= dat.replace(/　/g,'');
+		dat= dat.replace(/ /g, '');
+		if( dat.match(ptrn) ){                    //都道府県/
+			ans= elm[ii].htmlFor;
+			break;
+	}	}
+	return ans;
 }
