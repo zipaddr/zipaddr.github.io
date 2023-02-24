d836121bfd1dbcbad9ed6808b8bc33f5359d2fc5
ta_terunuma <ta.terunuma@gmail.com>
2023-02-25 06:25:28 +0900

    ninjaforms.js_1.6

--- ninjaforms.js
+++ ninjaforms.js
@@ -1,5 +1,5 @@
 function zipaddr_ownpm(){     //Ninja Forms用
-	const ver="1.5";
+	const ver="1.6";
 	let ans= new Array();
 	ans[0]= "zip";
 	ans[1]= zipaddr_ownpm_tag("label","都道府県");
@@ -33,17 +33,18 @@ function zipaddr_ownpm_tag(tag,names){
 	const elm= document.getElementsByTagName(tag);
 	for( let ii=0;ii<elm.length;ii++ ){
 		let dat= elm[ii].innerHTML;               // <label...>都道府県  </label>
-		dat= dat.replace(/　/g,'');
-		dat= dat.replace(/ /g, '');
+		dat= dat.replace(/　/g, '');
+		dat= dat.replace(/ /g,  '');
+		dat= dat.replace(/\s+/g,'');
 		if( dat.length < namel ){;}
 		else
 		if( dat.substr(0,namel) != pref ){;}      // 都道府県
 		else
-		if( dat == names ) {ans= elm[ii].id.replace('label-',''); break;} // nf-label-field-xx
+		if( dat == names )  {ans= elm[ii].getAttribute('for'); break;} // nf-field-xx
 		else{
 			let ddd= dat.substr(namel,1);         // 都道府県n
 			ddd= ("2" <= ddd && ddd <= "5") ?  pref+ddd : pref;
-			if( ddd == names ) {ans= elm[ii].id.replace('label-',''); break;}
+			if( ddd==names ){ans= elm[ii].getAttribute('for'); break;}
 	}	}
 	return ans;
 }
