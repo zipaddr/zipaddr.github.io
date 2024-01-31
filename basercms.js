a8b0b01eeb83acb7fd29c432391b9e28ebca454c
ta_terunuma <ta.terunuma@gmail.com>
2024-01-31 09:04:35 +0900

    basercms.js_1.6

--- basercms.js
+++ basercms.js
@@ -1,11 +1,11 @@
 function zipaddr_ownb(){     //basercms用
-  var ver="1.5";
+  var ver="1.6";
   ZP.pm[1]={"zip":"MailMessageZip", "zip1":"MailMessageZip1", "pref":"MailMessageAddress1", "city":"", "area":"", "addr":"MailMessageAddress2", "focus":"MailMessageAddress2"};
   ZP.pm[2]={"zip":"MessageZip", "zip1":"MessageZip1", "pref":"MessageAddress1", "city":"", "area":"", "addr":"MessageAddress2", "focus":"MessageAddress2"};
   ZP.pm[3]={"zip":"zip3","zip1":"zip31","pref":"pref3","city":"city3","area":"area3","addr":"addr3","focus":"addr3"};
   ZP.pm[4]={"zip":"zip4","zip1":"zip41","pref":"pref4","city":"city4","area":"area4","addr":"addr4","focus":"addr4"};
   ZP.pm[5]={"zip":"zip5","zip1":"zip51","pref":"pref5","city":"city5","area":"area5","addr":"addr5","focus":"addr5"};
-  ZP.pm[6]={"zip":"zip6","zip1":"zip61","pref":"pref6","city":"city6","area":"area6","addr":"addr6","focus":"addr6"};
+  ZP.pm[6]={"zip":"zip" ,"zip1":"zip1", "pref":"address_1","city":"city","area":"area","addr":"address_2","focus":"address_3"};
 //ZP.pm[7]={"zip":"〒",  "zip1":"下4桁","pref":"都道府県","city":"市区町村","area":"地区","addr":"番地","focus":"focus"};
 
   ZP.basercms= "1";
